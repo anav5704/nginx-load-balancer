a6cfe96f34db9d31a879bd6da7dc3dbdf34b39ab
Anav Chand <anavkki@gmail.com>
2025-02-16 13:50:05 +1200

    fix: spelling error

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const id = process.env.SEVER_ID, port = 3000
+const id = process.env.SERVER_ID,
+    port = 3000;
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text(`Hello NGINX ${id}`))
+app.get("/", (c) => c.text(`Hello NGINX ${id}`));
 
-console.log(`Server ${id} is running on http://localhost:${port}`)
+console.log(`Server ${id} is running on http://localhost:${port}`);
 
 serve({
     fetch: app.fetch,
-    port
-})
+    port,
+});
